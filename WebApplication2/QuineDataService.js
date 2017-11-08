b7d1263d2b2a6fb54021d10ff5fe01383dec5e90
ragar005 <ragar005@fiu.edu>
2017-11-08 18:51:57 -0500

    Added all files

---
+++ WebApplication2/QuineDataService.js
@@ -0,0 +1,14 @@
+﻿angular.module('Quine').service('QuineDataService',
+    function ($http, $window) {
+        var baseURL = '/API';
+        return {
+            GetQuine: function () {
+                return $http({
+                    method: 'GET',
+                    url: baseURL,
+                }).then(function (result) { return result.data; });
+            }
+        }
+    });
+
+
---
+++ WebApplication2/scripts/index.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("Quine", []);
+
+app.controller('quineController',
+    function ($scope, QuineDataService) {
+
+        $scope.changePageTitle = function () {
+            document.title = document.getElementById("page_title").value;
+            $scope.getQuine();
+        }
+
+        $scope.getQuine = function () {
+            QuineDataService.CheckLiveness(quineRequestModel).then(function (result) {
+                if (result != null) {
+
+                }
+            });
+        }
+    });
